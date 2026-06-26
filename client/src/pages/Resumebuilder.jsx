804d237d33113cfd164941f32d39baf565a4faf9
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2026-06-26 19:43:23 +0530

    Exp section added

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -17,6 +17,7 @@ import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
+import ExperienceForm from '../components/ExperienceForm'
 
 const Resumebuilder = () => {
 
@@ -127,6 +128,10 @@ const Resumebuilder = () => {
               {activeSection.id==='summary' && (
                 <ProfessionalSummaryForm data={resumeData.professional_summary}
                 onChange={(data)=>setResumeData(prev=>({...prev,professional_summary:data}))} setResumeData={setResumeData}/>
+              )}
+              {activeSection.id==='experience' && (
+                <ExperienceForm data={resumeData.experience}
+                onChange={(data)=>setResumeData(prev=>({...prev,experience:data}))} />
               )}
               
               
