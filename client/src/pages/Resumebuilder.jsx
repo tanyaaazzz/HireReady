2db190a1c0f33344f07c622c8a118fd50142d8eb
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2026-06-26 20:55:49 +0530

    Edu section added

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -18,6 +18,7 @@ import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
+import EducationForm from '../components/EducationForm'
 
 const Resumebuilder = () => {
 
@@ -132,6 +133,10 @@ const Resumebuilder = () => {
               {activeSection.id==='experience' && (
                 <ExperienceForm data={resumeData.experience}
                 onChange={(data)=>setResumeData(prev=>({...prev,experience:data}))} />
+              )}
+              {activeSection.id==='education' && (
+                <EducationForm data={resumeData.education}
+                onChange={(data)=>setResumeData(prev=>({...prev,education:data}))} />
               )}
               
               
