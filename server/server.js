edcd34df91d2434eb57e6f910caee6f5091d36cd
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2026-09-10 02:58:31 +0530

    initial server setup with db config

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+//Database connection
+await connectDB()
+const app= express();
+const PORT= process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cors())
+app.get('/',(req,res)=>res.send("server is live"))
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
