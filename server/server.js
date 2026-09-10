3111086014aad6b124222ea78dfcc45072a8a69f
Tanya Aggarwal <tanya.aggarwal106@gmail.com>
2026-09-10 19:10:00 +0530

    user auth api

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 //Database connection
 await connectDB()
 const app= express();
@@ -10,6 +11,7 @@ const PORT= process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors())
 app.get('/',(req,res)=>res.send("server is live"))
+app.use('/api/users',userRouter)
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
